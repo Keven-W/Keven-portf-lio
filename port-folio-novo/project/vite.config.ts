796ad92939e3cad69e8887e988485eefd6c8306d
Keven-W <keven.w.1107@gmail.com>
2025-11-07 13:42:34 -0300

    preparando para lançar

--- port-folio-novo/project/vite.config.ts
+++ port-folio-novo/project/vite.config.ts
@@ -1,10 +1,8 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
+// substitua /repo-name/ pelo nome do seu repo
 export default defineConfig({
+  base: '/Keven-portf-lio/',
   plugins: [react()],
-  optimizeDeps: {
-    exclude: ['lucide-react'],
-  },
-});
+})
