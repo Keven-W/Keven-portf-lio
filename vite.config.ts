7740fb0ee65f2c2b14728300207c68533e8d9ad0
Keven-W <keven.w.1107@gmail.com>
2025-12-09 18:31:57 -0300

    mudanças para o funcionamento da página

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Keven-porf-lio/', // ALTERE COM O NOME DO SEU REPOSITÓRIO
+  base: '/Keven-porf-lio/', 
 });
